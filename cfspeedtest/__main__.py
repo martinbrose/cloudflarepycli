6e75c8c4b221ac5e0452c333f875c1a35a537972
Nytelife26 <xtylerjrx@gmail.com>
2024-06-15 00:48:02 +0100

    refactor: re-implement argparse

--- cfspeedtest/__main__.py
+++ cfspeedtest/__main__.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+from argparse import ArgumentParser
 
 from cfspeedtest.cloudflare import CloudflareSpeedtest
 from cfspeedtest.logger import log, set_verbosity
@@ -10,19 +11,28 @@ from cfspeedtest.version import __version__
 
 def cfspeedtest() -> None:
     """Run a network speedtest suite via Cloudflare."""
-    args = sys.argv[1:]
+    parser = ArgumentParser(prog="cfspeedtest", description=cfspeedtest.__doc__)
+    parser.add_argument("--debug", action="store_true", help="Log network I/O.")
+    parser.add_argument(
+        "--json", action="store_true", help="Write JSON to stdout."
+    )
+    parser.add_argument(
+        "--version",
+        action="store_true",
+        help="Show program's version and exit.",
+    )
+    args = parser.parse_args()
 
-    set_verbosity(debug="--debug" in args)
+    set_verbosity(debug=args.debug)
 
-    if "--version" in args:
+    if args.version:
         log.info("cfspeedtest %s", __version__)
         log.debug("Python %s", sys.version)
         sys.exit(0)
 
-    use_json = "--json" in args
-    results = CloudflareSpeedtest(logger=None if use_json else log).run_all()
+    results = CloudflareSpeedtest(logger=None if args.json else log).run_all()
 
-    if use_json:
+    if args.json:
         log.info(json.dumps(CloudflareSpeedtest.results_to_dict(results)))
 
 
