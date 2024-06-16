6f9fd1c297b67b076671802875e916afa61b7c23
Nytelife26 <xtylerjrx@gmail.com>
2024-06-16 23:33:42 +0100

    refactor: export essential types

--- cfspeedtest/__init__.py
+++ cfspeedtest/__init__.py
@@ -1,1 +1,5 @@
 """cfspeedtest, a Cloudflare speedtest suite in Python."""
+
+from cfspeedtest.cloudflare import CloudflareSpeedtest, TestSpec, TestType
+
+__all__ = ("CloudflareSpeedtest", "TestSpec", "TestType")
